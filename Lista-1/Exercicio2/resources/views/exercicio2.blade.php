b38b6d6809a857d21908e5bfddaa1aa27cbfb253
Nicolas Santos <santosnicolas026@gmail.com>
2025-08-29 14:20:10 -0300

    Lista 1 - Exercício 2

--- Lista-1/Exercicio2/resources/views/exercicio2.blade.php
+++ Lista-1/Exercicio2/resources/views/exercicio2.blade.php
@@ -11,7 +11,7 @@
 <body>
     <div class="container py-3">
         <h1>Lista 1 - Exercício 2 (Subtração)</h1>
-        <form method="post" action="respExercicio2">
+        <form method="post" action="/respExercicio2">
             @CSRF
             <div class="mb-3">
                 <label for="num1" class="form-label">Insira o primeiro número</label>
